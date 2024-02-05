b515a3df09cfaef1d686dbfb806fe5854a43fc8a
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-02-05 13:51:41 +0100

    add: main and app desings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AuthLayout from "./pages/_auth/AuthLayout";
+import SignInForm from "./pages/_auth/forms/SignInPage";
+import SignUpForm from "./pages/_auth/forms/SignUpPage";
+
+import UserPageLayout from "./pages/_userpages/UserPageLayout";
+import UserPage from "./pages/_userpages/UserPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route element={<AuthLayout />}>
+            <Route index element={<SignInForm />} />
+            <Route path="sign-up" element={<SignUpForm />} />
+          </Route>
+          {/* Private Routes */}
+          <Route element={<UserPageLayout />}>
+            <Route path="user" element={<UserPage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+//import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { ApolloClient, createHttpLink, ApolloProvider } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import { cache } from "./lib/cache";
+import "./index.css";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const authLink = setContext(() => {
+  const token = JSON.parse(localStorage.getItem("authUser")).token;
+  console.log("JWT", token);
+
+  return {
+    headers: {
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: cache,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>
+);

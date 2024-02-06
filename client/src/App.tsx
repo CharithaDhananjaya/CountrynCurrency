0c8cbd3154009332e4fca1e78ee7bbdf9c8afab3
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-02-06 15:56:06 +0100

    updated: Authprovider wrapped

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./hooks/useAuth";
+
 import AuthLayout from "./pages/_auth/AuthLayout";
 import SignInForm from "./pages/_auth/forms/SignInPage";
 import SignUpForm from "./pages/_auth/forms/SignUpPage";
@@ -11,17 +13,19 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route element={<AuthLayout />}>
-            <Route index element={<SignInForm />} />
-            <Route path="/sign-up" element={<SignUpForm />} />
-          </Route>
-          {/* Private Routes */}
-          <Route element={<UserPageLayout />}>
-            <Route path="/user" element={<UserPage />}></Route>
-          </Route>
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            {/* Public Routes */}
+            <Route element={<AuthLayout />}>
+              <Route index element={<SignInForm />} />
+              <Route path="/sign-up" element={<SignUpForm />} />
+            </Route>
+            {/* Private Routes */}
+            <Route element={<UserPageLayout />}>
+              <Route path="/user" element={<UserPage />}></Route>
+            </Route>
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </>
   );
